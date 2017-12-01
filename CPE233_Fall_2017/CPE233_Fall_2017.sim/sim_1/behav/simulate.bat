a161f6808b44fb7fc50116578dd9066019db55df
oousterman <oousterman@yahoo.com>
2017-12-01 15:16:24 -0800

    nothing, trying to figure out prog rom missing

--- CPE233_Fall_2017/CPE233_Fall_2017.sim/sim_1/behav/simulate.bat
+++ CPE233_Fall_2017/CPE233_Fall_2017.sim/sim_1/behav/simulate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xsim tb_RAT_wrapper_behav -key {Behavioral:sim_1:Functional:tb_RAT_wrapper} -tclbatch tb_RAT_wrapper.tcl -view C:/Users/afergu06/Documents/GitHub/RAT_MCU/CPE233_Fall_2017/RAT_wrapper_behav.wcfg -log simulate.log
+call %xv_path%/xsim tb_RAT_wrapper_behav -key {Behavioral:sim_1:Functional:tb_RAT_wrapper} -tclbatch tb_RAT_wrapper.tcl -view C:/Users/oousterm/Documents/GitHub/RAT_MCU/CPE233_Fall_2017/RAT_wrapper_behav.wcfg -log simulate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
